8e608c5cb8a228f559f9ec3b3274a77b1d598716
Chaithep <sound014@gmail.com>
2018-01-19 13:38:21 +0700

    Add Marker on a Map.

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -11,7 +11,8 @@ class PickLocation extends Component {
                 Dimensions.get("window").width /
                 Dimensions.get("window").height *
                 0.0122
-        }
+        },
+        locationChosen: false
     };
     pickLocationHandler = event => {
         const coords = event.nativeEvent.coordinate;
@@ -21,11 +22,16 @@ class PickLocation extends Component {
                     ...prevState.focusedLocation,
                     latitude: coords.latitude,
                     longitude: coords.longitude
-                }
+                },
+                locationChosen: true
             };
         });
     }
     render() {
+        let marker = null;
+        if (this.state.locationChosen) {
+            marker = <MapView.Marker coordinate={this.state.focusedLocation} />
+        }
         return (
             <View style={styles.container}>
                 <MapView
@@ -33,7 +39,9 @@ class PickLocation extends Component {
                     region={this.state.focusedLocation}
                     style={styles.map}
                     onPress={this.pickLocationHandler}
-                />
+                >
+                    {marker}
+                </MapView>
                 <View style={styles.button}>
                     <Button title="Locate Me" onPress={() => alert("Pick Location!")} />
                 </View>
