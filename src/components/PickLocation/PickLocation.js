724f32c7d62df6fb35452a288ddf48be742ef2bf
Chaithep <sound014@gmail.com>
2018-01-19 15:58:21 +0700

    Add permission on App and get User Location.

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -32,6 +32,25 @@ class PickLocation extends Component {
             };
         });
     }
+    getLocationHandler = () => {
+        navigator.geolocation.getCurrentPosition(
+            pos => {
+                const coordsEvent = {
+                    nativeEvent: {
+                        coordinate: {
+                            latitude: pos.coords.latitude,
+                            longitude: pos.coords.longitude
+                        }
+                    }
+                };
+                this.pickLocationHandler(coordsEvent);
+            },
+            err => {
+                console.log(err);
+                alert("Feching the Position failed, please pick one manually!");
+            }
+        );
+    }
     render() {
         let marker = null;
         if (this.state.locationChosen) {
@@ -49,7 +68,7 @@ class PickLocation extends Component {
                     {marker}
                 </MapView>
                 <View style={styles.button}>
-                    <Button title="Locate Me" onPress={() => alert("Pick Location!")} />
+                    <Button title="Locate Me" onPress={this.getLocationHandler} />
                 </View>
             </View>
         );
