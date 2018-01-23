f09af97bcb67192c5aab9a43c34a8531df5a32d9
Chaithep <sound014@gmail.com>
2018-01-23 14:41:14 +0700

    Install and cofiguration Redux-Thunk

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,5 +1,5 @@
-import { createStore, combineReducers, compose } from 'redux';
-
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import placesReducer from './reducers/places';
 
 let composeEnhancers = compose;
@@ -13,7 +13,7 @@ const rootReducer = combineReducers({
 });
 
 const configStore = () => {
-    return createStore(rootReducer,composeEnhancers());
+    return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 };
 
 export default configStore;
