dee97f93c6ce0fe1d2cb72e09c6f02026698ef22
Chaithep <sound014@gmail.com>
2018-01-24 10:35:49 +0700

    Install firebase tool and Storing Image to firebase storage by firebase function.

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,12 +1,32 @@
 import { ADD_PLACE, DELETE_PLACE, DESELECT_PLACE, SELECT_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-    return {
-        type: ADD_PLACE,
-        placeName: placeName,
-        location: location,
-        image: image
-    };
+    return dispatch => {
+        const placeData = {
+            name: placeName,
+            location: location
+        };
+        fetch("https://us-central1-awesome-place-1516267937295.cloudfunctions.net/storeImage", {
+            method: "POST",
+            body: JSON.stringify({
+                image: image.base64
+            })
+        })
+            .catch(err => console.log(err))
+            .then(res => res.json())
+            .then(parsedRes => {
+                console.log(parsedRes);
+            });
+        // fetch("https://awesome-place-1516267937295.firebaseio.com/places.json", {
+        //     method: "POST",
+        //     body: JSON.stringify(placeData)
+        // })
+        //     .catch(err => console.log(err))
+        //     .then(res => res.json())
+        //     .then(parsedRes => {
+        //         console.log(parsedRes);
+        //     });
+    }
 };
 
 export const deletePlace = key => {
@@ -15,3 +35,10 @@ export const deletePlace = key => {
         placeKey: key
     };
 };
+
+// return {
+//     type: ADD_PLACE,
+//     placeName: placeName,
+//     location: location,
+//     image: image
+// };
